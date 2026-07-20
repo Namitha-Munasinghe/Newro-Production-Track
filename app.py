a027572958c9da19ae5866af726dd94595977ade
Namitha-Munasinghe <namithamunasinghe13@gmail.com>
2026-07-20 16:01:37 +0530

    app.py cleanup

--- app.py
+++ app.py
@@ -1,28 +1,3 @@
-# from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
-
-
-# app = Flask(__name__)
-
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newro_poultry.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-# with app.app_context():
-#     db.create_all()
-
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
